dffa402cf72706fa18a515b1f876d683fe2a153d
Eternal <2457817471@qq.com>
2023-03-14 08:40:58 +0800

    hot-fix commit1

--- src/main/java/com/ztc/App.java
+++ src/main/java/com/ztc/App.java
@@ -9,5 +9,6 @@ public class App {
         System.out.println("Hello World!");
         System.out.println("Hello gitTest02");
         System.out.println("Hello gitTest03");
+        System.out.println("Hello gitTest04");
     }
 }
